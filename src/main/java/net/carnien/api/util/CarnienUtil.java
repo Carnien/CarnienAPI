24cd5c7c05e38d5d16fc9da5aa6e3cf8b5498c5d
Carni <carnivohra@gmail.com>
2024-04-30 22:01:42 +0200

    Add CarnienUtil documentation

--- src/main/java/net/carnien/api/util/CarnienUtil.java
+++ src/main/java/net/carnien/api/util/CarnienUtil.java
@@ -6,12 +6,25 @@ public class CarnienUtil {
 
     private final Carnien carnien;
 
+    /**
+     * A class contained by a Carnien field
+     * with a getter. Created in order to save
+     * the Carnien getter in classes which extends
+     * this util class.
+     *
+     * @param carnien Carnien plugin instance.
+     */
     public CarnienUtil(Carnien carnien) {
         this.carnien = carnien;
     }
 
+    /**
+     * Returns the current Carnien plugin instance.
+     *
+     * @return Carnien plugin instance.
+     */
     protected Carnien getCarnien() {
-        return  carnien;
+        return carnien;
     }
 
 }
