2a1e7e4ce945813f3a77364dcc5ba2ca41cd07f4
Carni <carnivohra@gmail.com>
2024-04-30 22:17:20 +0200

    Add CustomConfig documentation

--- src/main/java/net/carnien/api/util/CustomConfig.java
+++ src/main/java/net/carnien/api/util/CustomConfig.java
@@ -9,6 +9,14 @@ import java.io.IOException;
 
 public class CustomConfig {
 
+    /**
+     * Creates a custom yaml configuration file.
+     * File will be generated in plugin's directory
+     *
+     * @param plugin Owner of the file.
+     * @param name Name of the file. No need to end it like ".yml"
+     * @return The file which has been created.
+     */
     public static File create(JavaPlugin plugin, String name) {
         final File pluginDirectory = plugin.getDataFolder();
         final File customFile = new File(pluginDirectory, name);
@@ -21,10 +29,24 @@ public class CustomConfig {
         return customFile;
     }
 
+
+    /**
+     * Loads the content of a file into a
+     * FileConfiguration.
+     *
+     * @param file File to load.
+     * @return A loaded file converted into a FileConfiguration
+     */
     public static FileConfiguration get(File file) {
         return YamlConfiguration.loadConfiguration(file);
     }
 
+    /**
+     * Saves a FileConfiguration into a file.
+     *
+     * @param file The file to save.
+     * @param config The FileConfiguration with all the content.
+     */
     public static void save(File file, FileConfiguration config) {
         try { config.save(file); } catch (IOException ignored) { }
     }
